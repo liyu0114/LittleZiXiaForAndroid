7305709e3d1c72e20f58301d1c26b887b8ba5c59
Liyu <liyu@example.com>
2026-05-06 13:50:53 +0800

    v1.0.246: 版本升级到v1.0.170

--- lib/config/app_version.dart
+++ lib/config/app_version.dart
@@ -3,9 +3,9 @@
 // 统一管理版本号，避免不一致
 
 class AppVersion {
-  static const String version = '1.0.169';
-  static const String buildNumber = '169';
-  static const String fullVersion = 'v1.0.169';
+  static const String version = '1.0.170';
+  static const String buildNumber = '170';
+  static const String fullVersion = 'v1.0.170';
   static const String buildDate = '2026-05-06';
   
   static String get displayVersion => version;
